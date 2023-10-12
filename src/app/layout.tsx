20f40179931eeb34bef704a954764362f0f0416e
anuitexDev <dev@anuitex.net>
2023-10-12 17:52:54 +0300

    updated cv, favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import type { Metadata } from "next";
 import { poppins } from "./font";
 import { Footer, Header, ScrollUp } from "@/components";
 
+import favicon from "../../public/favicon.ico";
+
 export const metadata: Metadata = {
   title: "Eugene Chekerdes",
   description:
@@ -15,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={poppins.variable}>
       <head>
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css" />
         <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" />
       </head>

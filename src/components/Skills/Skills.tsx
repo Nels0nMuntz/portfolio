91dfe1294c91408d6fc0db117d0b33f0af7b9e1e
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2024-03-23 14:13:26 -0700

    updated skills info

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -8,14 +8,23 @@ export default function Skills() {
         <span className="section-subtitle">My technical level</span>
         <div className="flex justify-center">
           <div className="px-4 xs:px-6 sm:px-16 py-4 xs:py-6 sm:py-8 border border-primary rounded-2xl bg-light-2">
-            <h3 className="text-body-1 font-medium text-center mb-6">Frontend development</h3>
+            {/* <h3 className="text-body-1 font-medium text-center mb-6">Frontend development</h3> */}
             <div className="flex justify-center gap-10">
               <div className="flex flex-col items-start gap-4">
                 <SkillItem title="HTML" subtitle="Advanced" />
                 <SkillItem title="CSS" subtitle="Advanced" />
-                <SkillItem title="SASS/SCSS" subtitle="Intermediate" />
+                <SkillItem title="SASS/SCSS" subtitle="Advanced" />
                 <SkillItem title="Bootstrap" subtitle="Intermediate" />
-                <SkillItem title="Tailwindcss" subtitle="Intermediate" />
+                <SkillItem title="Tailwindcss" subtitle="Advanced" />
+                <SkillItem title="GSAP" subtitle="Intermediate" />
+              </div>
+              <div className="flex flex-col items-start gap-4">
+                <SkillItem title="JavaScript" subtitle="Advanced" />
+                <SkillItem title="TypeScript" subtitle="Advanced" />
+                <SkillItem title="Webpack" subtitle="Advanced" />
+                <SkillItem title="Vite" subtitle="Intermediate" />
+                <SkillItem title="Gulp" subtitle="Advanced" />
+                <SkillItem title="Git" subtitle="Advanced" />
               </div>
               <div className="flex flex-col items-start gap-4">
                 <SkillItem title="React.js" subtitle="Advanced" />
@@ -23,13 +32,12 @@ export default function Skills() {
                 <SkillItem title="Redux" subtitle="Advanced" />
                 <SkillItem title="MobX" subtitle="Intermediate" />
                 <SkillItem title="Material UI" subtitle="Advanced" />
+                <SkillItem title="Storybook" subtitle="Intermediate" />
               </div>
               <div className="flex flex-col items-start gap-4">
-                <SkillItem title="JavaScript" subtitle="Advanced" />
-                <SkillItem title="TypeScript" subtitle="Intermediate" />
-                <SkillItem title="Webpack" subtitle="Intermediate" />
-                <SkillItem title="Gulp" subtitle="Advanced" />
-                <SkillItem title="Git" subtitle="Intermediate" />
+                <SkillItem title="Docker" subtitle="Intermediate" />
+                <SkillItem title="GitLab" subtitle="Intermediate" />
+                <SkillItem title="AWS" subtitle="Intermediate" />
               </div>
             </div>
           </div>
